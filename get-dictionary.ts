3868c3250b3a3d98081a92fd93afc2c270287e23
rafaeelricco <rafaelricco10@gmail.com>
2024-12-24 21:09:39 -0300

    refactor: improve code formatting and structure across multiple components
    - Reformatted the getDictionary function for better readability.
    - Updated Tailwind configuration for improved organization and clarity.
    - Enhanced TypeScript configuration for better path resolution and structure.
    - Refactored AnimatedGridPattern component to streamline code and improve readability.
    - Added URL display functionality in SafariFrame component for enhanced user experience.

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -6,4 +6,5 @@ const dictionaries = {
    pt: () => import('@/dictionaries/pt.json').then((module) => module.default)
 }
 
-export const getDictionary = async (locale: Locale) => dictionaries[locale]?.() ?? dictionaries.pt()
+export const getDictionary = async (locale: Locale) =>
+   dictionaries[locale]?.() ?? dictionaries.pt()
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,57 +11,51 @@ const config: Config = {
       './src/features/**/*.{js,ts,jsx,tsx,mdx}'
    ],
    theme: {
-   	extend: {
-   		container: {
-   			center: true,
-   			padding: '1rem',
-   			screens: {
-   				'2xl': '1636px'
-   			}
-   		},
-   		fontFamily: {
-   			sans: [
-   				'Inter',
-   				'sans-serif'
-   			],
-   			inter: [
-   				'Inter',
-   				'sans-serif'
-   			]
-   		},
-   		colors: {
-   			black: {
-   				primary: '#34383A'
-   			},
-   			gray: {
-   				primary: '#868686'
-   			},
-   			gradient: {
-   				start: '#8a30e3',
-   				middle: '#ee6ba1',
-   				end: '#eac893'
-   			}
-   		},
-   		animation: {
-   			'shiny-text': 'shiny-text 8s infinite',
-   			gradient: 'gradient 8s linear infinite'
-   		},
-   		keyframes: {
-   			'shiny-text': {
-   				'0%, 90%, 100%': {
-   					'background-position': 'calc(-100% - var(--shiny-width)) 0'
-   				},
-   				'30%, 60%': {
-   					'background-position': 'calc(100% + var(--shiny-width)) 0'
-   				}
-   			},
-   			gradient: {
-   				to: {
-   					backgroundPosition: 'var(--bg-size) 0'
-   				}
-   			}
-   		}
-   	}
+      extend: {
+         container: {
+            center: true,
+            padding: '1rem',
+            screens: {
+               '2xl': '1636px'
+            }
+         },
+         fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+            inter: ['Inter', 'sans-serif']
+         },
+         colors: {
+            black: {
+               primary: '#34383A'
+            },
+            gray: {
+               primary: '#868686'
+            },
+            gradient: {
+               start: '#8a30e3',
+               middle: '#ee6ba1',
+               end: '#eac893'
+            }
+         },
+         animation: {
+            'shiny-text': 'shiny-text 8s infinite',
+            gradient: 'gradient 8s linear infinite'
+         },
+         keyframes: {
+            'shiny-text': {
+               '0%, 90%, 100%': {
+                  'background-position': 'calc(-100% - var(--shiny-width)) 0'
+               },
+               '30%, 60%': {
+                  'background-position': 'calc(100% + var(--shiny-width)) 0'
+               }
+            },
+            gradient: {
+               to: {
+                  backgroundPosition: 'var(--bg-size) 0'
+               }
+            }
+         }
+      }
    },
    plugins: [tailwindcssAnimate]
 }

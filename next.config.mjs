4701d369e8e1f7eaffd34a8700fcaaafa5f0b38d
rafaeelricco <rafaelricco10@gmail.com>
2024-12-31 11:26:25 -0300

    feat: add markdown support with raw-loader and type definitions
    - Configured webpack to handle markdown files using raw-loader, allowing for the import of markdown content as strings.
    - Added new type definitions for markdown files to improve TypeScript support and ensure proper handling of markdown imports.
    - Updated package.json and pnpm-lock.yaml to include necessary dependencies for markdown processing, enhancing the project's capabilities for content management.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   webpack: (config) => {
+      config.module.rules.push({
+         test: /\.md$/,
+         use: 'raw-loader'
+      })
+      return config
+   },
    async redirects() {
       return [
          {
